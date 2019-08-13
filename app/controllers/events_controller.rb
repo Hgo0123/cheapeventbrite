e7383ae8ee5592f2f73bee1c69202bb4c6151357
Hgo0123 <hcordillot@gmail.com>
2019-08-13 19:04:00 +0200

    change2 for heroku

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,7 +10,7 @@ class EventsController < ApplicationController
 
 	def create
 		@user = current_user.id
-		@new_event = Event.new('start_date' => params[:start_date], 'duration' => params[:duration], 'title' => params[:title], 'description' => params[:description], 'price' => params[:price], 'location' => params[:location], 'admin' => @user)
+		@new_event = Event.new('start_date' => params[:start_date], 'duration' => params[:duration], 'title' => params[:title], 'description' => params[:description], 'price' => params[:price], 'location' => params[:location], 'admin_id' => @user)
 		if @new_event.save
 			redirect_to root_path, alert: 'Votre événement a bien été créé'
 		else
